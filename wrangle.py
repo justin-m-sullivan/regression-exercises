2d6157353c9caba6fa4b6719ff882a2464bb0477
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-03-12 14:06:20 -0600

    adding in test, validate, split fx

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+from sklearn.model_selection import train_test_split
 from env import host, user, password
 
 # Establish a connection
@@ -39,6 +40,9 @@ def clean_telco_data(df):
     #Convert total_charges to float
     df.total_charges = df.total_charges.astype('float64')
 
+    #Reset index
+    df.set_index('customer_id', drop=True, inplace=True)
+
     return df
 
 def wrangle_telco():
@@ -53,3 +57,18 @@ def wrangle_telco():
 
     return df
 
+def train_validate_test_split(df, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed)
+
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed)
+    return train, validate, test
